3f801b98b2f45083dd618024ec23aeac4fca5989
David Demaree <david@demaree.me>
2021-06-06 17:09:55 -0400

    Add notes section backed by WP data

--- src/_includes/components/article-card.vue
+++ src/_includes/components/article-card.vue
@@ -3,7 +3,7 @@
     <a :href="link" class="space-y-1 flex-1">
       <h2 class="dd-heading-2">{{ title }}</h2>
       <p v-if="excerpt" class="max-w-[40ch]">{{ excerpt }}</p>
-      <p class="font-medium text-sm">{{ date }}</p>
+      <p class="font-medium text-sm">{{ this.prettyDate(date) }}</p>
     </a>
     <a :href="link" v-if="thumbnail">
       <article-thumbnail :src="thumbnail" class="sm:w-40 sm:h-30" />
---
+++ src/_templates/notes-list.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <header class="w-full-inset text-center mx-auto py-12">
+      <h1 class="dd-title">Notes</h1>
+    </header>
+    <div v-for="note in notes" :key="note.id" class="dd-prose">
+      <p class="mb-4 text-ink-medium uppercase tracking-wider">{{ formatDate(note.date_gmt) }}</p>
+      <h2 class="dd-heading-2 mt-0" v-html="note.title.rendered"></h2>
+      <div class="dd-prose" v-html="note.content.rendered"></div>
+      <hr />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      layout: 'layouts/base.njk',
+      pagination: {
+        data: 'wpNotes',
+        size: 10,
+        alias: 'notes'
+      },
+      permalink: data => {
+        if(data.pagination.pageNumber > 0)
+          return `/notes/${data.pagination.pageNumber + 1}/index.html`;
+        else
+          return `/notes/index.html`;
+      },
+    }
+  },
+  methods: {
+    formatDate(date) {
+      return this.prettyDate(date)
+    }
+  },
+  computed: {
+    _notes() {
+      console.log(this.note)
+      return []
+    }
+  }
+}
+</script>
