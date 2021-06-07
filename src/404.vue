9638b6382528914072aa5d0bf91b1bdfd91c9c29
David Demaree <david@demaree.me>
2021-06-07 16:40:26 -0400

    Add 404 page and redirects

---
+++ src/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="flex flex-col items-center justify-center gap-4">
+    <p class="text-5xl font-light">404</p>
+    <p>Sorry, page not found</p>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      gridwrap: false,
+      layout: 'layouts/base.njk',
+      permalink: '/404.html'
+    }
+  }
+}
+</script>
