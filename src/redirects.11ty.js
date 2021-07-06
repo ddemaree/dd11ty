eea53a7b12241b0da9ec43591894507a309411b3
David Demaree <david@demaree.me>
2021-07-05 20:48:13 -0400

    Remove blog stuff

--- src/redirects.11ty.js
+++ src/redirects.11ty.js
@@ -14,25 +14,6 @@ class Redirects {
       "/notes/:slug https://demaree.blog/p/:slug 301"
     ]
 
-    // data.wpNotes.forEach(note => {
-    //   rows.push(`/${note.slug} /notes/${note.slug} 301`)
-    // })
-    // data.collections.articles.forEach(article => {
-    //   const articleUrl = `/p/${article.fileSlug}`
-    //   rows.push(`/${article.fileSlug} ${articleUrl} 301`)
-
-    //   if(article.data.slug && article.data.slug !== article.fileSlug) {
-    //     rows.push(`/${article.data.slug} ${articleUrl} 301`)
-    //   }
-
-    //   if(article.data.aliases) {
-    //     article.data.aliases.forEach(alias => {
-    //       rows.push(`${alias} ${articleUrl} 301`)
-    //     })
-    //   }
-    // })
-    // console.log( data.collections.all.map(p => p.url) )
-
     return _.uniq(rows).join("\n")
   }
 }
