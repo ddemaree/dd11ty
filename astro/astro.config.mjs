2a904d80e3251e6adbd2a61ed98671caad3674a0
David Demaree <david@demaree.me>
2022-07-18 18:24:38 -0400

    WIP - added MDX

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"; // import preact from "@astrojs/preact";
 
-// import preact from "@astrojs/preact";
 import solid from "@astrojs/solid-js";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://demaree.me",
-  integrations: [solid(), tailwind()],
+  integrations: [solid(), tailwind(), mdx()]
 });
