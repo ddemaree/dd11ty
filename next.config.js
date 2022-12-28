8ef58cfde5c30e3e0dc659c6bf62223298a83729
David Demaree <david@demaree.me>
2022-12-27 23:19:18 -0500

    Try fixing canvas build error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+// Fix for node-canvas library include problem
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+}
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
