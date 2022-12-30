b8dcb9fd9415a6fc0e19d100d349bf1309f5aff1
David Demaree <david@demaree.me>
2022-12-29 23:13:03 -0500

    Put OG images back on blog posts

--- src/app/(blog)/post/[slug]/head.tsx
+++ src/app/(blog)/post/[slug]/head.tsx
@@ -11,12 +11,19 @@ export default async function Head({
   const { post } = await getSinglePost(slug);
   const { title, excerpt: subtitle, featuredImage } = post as WordpressPost;
 
+  const socialImageUrl = `/api/og-image?title=${encodeURIComponent(
+    title.replace(/ /g, "_").replace(/'/g, "%27")
+  )}&t=${Date.now()}`;
+
   return (
     <>
       <title>{`${title} • David Demaree's blog`}</title>
       <TitleTag title={`${title} • David Demaree's blog`} />
       <DescriptionTags description={subtitle} />
       <DefaultHeadTags />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:image" content={socialImageUrl} />
+      <meta name="twitter:image" content={socialImageUrl} />
     </>
   );
 }
