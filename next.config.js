cabbf3c00078afcb532f2db801043685c0250c22
David Demaree <david@demaree.me>
2023-04-14 07:42:47 -0400

    Remove stale config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-// Fix for node-canvas library include problem
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
-}
-
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
@@ -55,16 +42,6 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/feeds/posts.xml",
-        destination: "/feed/rss",
-        permanent: false,
-      },
-      {
-        source: "/feeds/posts.json",
-        destination: "/feed/json",
-        permanent: false,
-      },
-      {
         source: "/p/:slug",
         destination: "/post/:slug",
         permanent: false,
@@ -82,37 +59,13 @@ const nextConfig = {
         source: "/.well-known/webfinger",
         destination: "/api/webfinger",
       },
-      {
-        source: "/post/:slug/socialImage.png",
-        destination: "/api/post-image/:slug",
-      },
       // $parent_id/$revision_id/$type/$status/$nonce
       {
         source: "/_preview/:parentId/:id/:type/:status/:nonce",
         destination:
           "/_preview?parent=:parentId&id=:id&type=:type&status=:status&n=:nonce",
       },
-      {
-        source: "/_preview/:parentId/:id/:type/:status/json",
-        destination:
-          "/_preview/json?parent=:parentId&id=:id&type=:type&status=:status",
-      },
     ];
-  },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    // Important: return the modified config
-    config.externals = [
-      ...config.externals,
-      "jsdom",
-      "bufferutil",
-      "canvas",
-      "utf-8-validate",
-    ];
-
-    return config;
   },
 };
 
