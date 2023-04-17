3d4374405c2e3054e1b3e24813330654c4028faa
David Demaree <david@demaree.me>
2023-04-17 09:15:25 -0400

    Copied remaining files from Astro repo

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    assets: true
+  },
+  output: "server",
+  adapter: vercel({
+    analytics: true
+  }),
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), mdx()]
+});
