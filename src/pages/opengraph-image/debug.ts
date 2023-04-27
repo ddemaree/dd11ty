262d8f87d093c4f27582acf0a0d65ed281b40574
David Demaree <david@demaree.me>
2023-04-27 13:14:44 -0400

    Use glob to get all entries

--- src/pages/opengraph-image/debug.ts
+++ src/pages/opengraph-image/debug.ts
@@ -2,13 +2,15 @@ import { getRootPath } from "@/utils";
 import type { APIRoute } from "astro";
 import { getEntryBySlug } from "astro:content";
 import fs from "fs/promises";
+import { glob } from "glob";
 
-export const get: APIRoute = async ({ request }) => {
+export const get: APIRoute = async ({ request, params }) => {
   const rootPath = getRootPath();
   const sampleEntry = await getEntryBySlug("blog", "monkeys-typewriters");
   const sampleEntryImg = sampleEntry.data.cover;
 
-  const entries = await fs.readdir("./dist/chunks");
+  // const entries = await fs.readdir("./");
+  const entries = await glob("./**/*.{jpg,png}");
 
   const out = {
     entries,
