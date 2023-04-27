b7a92079dcf1fe18ad10a8f4b0ea7f6da5a16b3a
David Demaree <david@demaree.me>
2023-04-27 13:04:02 -0400

    Further refine debug output

--- src/pages/opengraph-image/debug.ts
+++ src/pages/opengraph-image/debug.ts
@@ -1,9 +1,22 @@
+import { getRootPath } from "@/utils";
 import type { APIRoute } from "astro";
+import { getEntryBySlug } from "astro:content";
 import fs from "fs/promises";
 
 export const get: APIRoute = async ({ request }) => {
-  const entries = await fs.readdir("./");
-  return new Response(JSON.stringify(entries, null, 2), {
+  const rootPath = getRootPath();
+  const sampleEntry = await getEntryBySlug("blog", "monkeys-typewriters");
+  const sampleEntryImg = sampleEntry.data.cover;
+
+  const entries = await fs.readdir("./dist/_astro");
+
+  const out = {
+    entries,
+    sampleEntryImg,
+    rootPath,
+  };
+
+  return new Response(JSON.stringify(out, null, 2), {
     headers: {
       "content-type": "application/json",
     },
