a511b7c558b75fc5309396c2e295486ec42f2a52
David Demaree <david@demaree.me>
2023-04-27 10:59:53 -0400

    Refine debug output

--- src/pages/opengraph-image/debug.ts
+++ src/pages/opengraph-image/debug.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from "astro";
 import fs from "fs/promises";
 
 export const get: APIRoute = async ({ request }) => {
-  const entries = await fs.readdir("../");
+  const entries = await fs.readdir("./");
   return new Response(JSON.stringify(entries, null, 2), {
     headers: {
       "content-type": "application/json",
