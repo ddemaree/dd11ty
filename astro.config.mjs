92d81d3e43ff42dcef803fb60ca8624472876f96
David Demaree <david@demaree.me>
2023-04-28 08:37:32 -0400

    Switch back to serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
