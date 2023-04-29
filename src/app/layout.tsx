9966d54ea54b653f3abec0bb037d9654b8de432e
David Demaree <david@demaree.me>
2023-04-28 20:53:06 -0400

    fix stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,12 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
-import { siteUrl } from "@lib/urls";
+import { siteUrl } from "@/utils";
 import SiteContainer from "@/app/SiteContainer";
-import ClipPaths from "@components/ClipPaths";
-import { ThemeProvider, ThemeScript } from "@components/Theme";
+import { ThemeProvider, ThemeScript } from "@/components/Theme";
 import MainNavigation from "./MainNavigation";
-import "../styles/blog.css";
+
+import "../styles/main.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl("/", true)),
@@ -57,5 +57,22 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
+  );
+}
+
+export default function ClipPaths() {
+  return (
+    <svg width="10" height="10" viewBox="0 0 10 10">
+      <clipPath id="squircleClip" clipPathUnits="objectBoundingBox">
+        <path
+          fill="red"
+          stroke="none"
+          d="M 0,0.5 C 0,0 0,0 0.5,0 S 1,0 1,0.5 1,1 0.5,1 0,1 0,0.5"
+        />
+      </clipPath>
+      <clipPath id="squircleBoxClip" clipPathUnits="objectBoundingBox">
+        <circle cx="5" cy="5" r="5" fill="red" />
+      </clipPath>
+    </svg>
   );
 }
